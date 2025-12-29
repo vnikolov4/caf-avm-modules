0b656fad4a9d4098c2be259c740d19eb810548af
Vasil Nikolov <va.nik@hotmail.com>
2025-12-29 10:15:47 +0200

    Squashed 'avm-res-authorization-roleassignment/' content from commit b704ce0
    git-subtree-dir: avm-res-authorization-roleassignment
    git-subtree-split: b704ce0afb6b422ffeea7be42d3c2a75d289db88

---
+++ examples/default/outputs.tf
@@ -0,0 +1,59 @@
+output "all_principals" {
+  description = "All principals"
+  value       = module.role_assignments.all_principals
+}
+
+output "app_registrations" {
+  description = "Entra ID application registrations"
+  value       = module.role_assignments.app_registrations
+}
+
+output "entra_id_role_assignments" {
+  description = "Entra ID role assignments"
+  value       = module.role_assignments.entra_id_role_assignments
+}
+
+output "entra_id_role_definitions" {
+  description = "Entra ID role definitions"
+  value       = module.role_assignments.entra_id_role_definitions
+}
+
+output "groups" {
+  description = "Entra ID groups"
+  value       = module.role_assignments.groups
+}
+
+output "role_assignments" {
+  description = "Azure Resource Manager role assignments"
+  value       = module.role_assignments.role_assignments
+}
+
+output "role_defintions" {
+  description = "Azure Resource Manager role definitions"
+  value       = module.role_assignments.role_defintions
+}
+
+output "system_assigned_managed_identities" {
+  description = "System assigned managed identities"
+  value       = module.role_assignments.system_assigned_managed_identities
+}
+
+output "test_resource_ids" {
+  description = "Test resource ids"
+  value = {
+    management_group = azurerm_management_group.test.id
+    subscription     = "/subscriptions/${var.alternative_subscription_id}"
+    resource_group   = azurerm_resource_group.test.id
+    resource         = azurerm_static_web_app.test[local.system_assigned_managed_identities.sami1].id
+  }
+}
+
+output "user_assigned_managed_identities" {
+  description = "User assigned managed identities"
+  value       = module.role_assignments.user_assigned_managed_identities
+}
+
+output "users" {
+  description = "Entra ID users"
+  value       = module.role_assignments.users
+}
---
+++ examples/default/terraform.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> 1.6"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.46, < 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7, < 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.7"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "alternative"
+  subscription_id = var.alternative_subscription_id
+  features {}
+}
